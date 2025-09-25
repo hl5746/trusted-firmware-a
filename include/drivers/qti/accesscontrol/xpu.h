07bd0b0c7174fc8c7f58bc3296e03008f1029ba8
Sumit Garg <sumit.garg@oss.qualcomm.com>
2025-09-25 14:51:15 +0530

    feat(drivers/qti): introduce basic XPU driver
    Introduce basic XPU access control driver which allows currently to
    bypass XPU access control until a proper XPU driver is added upstream.
    
    Change-Id: I2b5ad50c57b0112302d3568e0e0bcf2116d3e259
    Co-developed-by: Casey Connolly <casey.connolly@linaro.org>
    Signed-off-by: Casey Connolly <casey.connolly@linaro.org>
    Signed-off-by: Sumit Garg <sumit.garg@oss.qualcomm.com>

---
+++ include/drivers/qti/accesscontrol/xpu.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef QTI_XPU_H
+#define QTI_XPU_H
+
+#include <stdint.h>
+
+int qti_xpu_bypass();
+
+#endif /* QTI_XPU_H */
