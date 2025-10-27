2a804980dcc40efdbdcdfc2a66e64d99371ac3a1
Jorge Ramirez-Ortiz <jorge.ramirez@oss.qualcomm.com>
2025-10-27 17:38:08 +0100

    feat(qti): integrate qtimer stub
    Add the qtimer driver stub and hook it into the generic QTI driver
    framework. The stub remains disabled while QTISECLIB continues to
    provide the active implementation.
    
    For coverity purposes, enable the driver on the non-bootable build for
    the kodiak/rb3gen2 platform.
    
    Change-Id: Icd93ca442b473a7c38bbbb203d820497cf3d1ed3
    Signed-off-by: Jorge Ramirez-Ortiz <jorge.ramirez@oss.qualcomm.com>

--- plat/qti/common/src/qti_bl31_setup.c
+++ plat/qti/common/src/qti_bl31_setup.c
@@ -14,6 +14,7 @@
 #include <drivers/generic_delay_timer.h>
 #include <drivers/qti/accesscontrol/accesscontrol.h>
 #include <drivers/qti/accesscontrol/xpu.h>
+#include <drivers/qti/qtimer/qtimer.h>
 #include <drivers/qti/sec_core/sec_core.h>
 #include <lib/bl_aux_params/bl_aux_params.h>
 #include <lib/coreboot.h>
@@ -103,6 +104,7 @@ void bl31_platform_setup(void)
 	qti_smmu_init();
 	qti_interrupt_svc_init(bl32_image_ep_info.pc != 0);
 	qti_sec_core_init();
+	qti_qtimer_init();
 	qtiseclib_bl31_platform_setup();
 
 	/* set boot state to cold boot complete. */
