631bd33590f1393d279cd2dee7dc100434777760
Hailey Liney <hliney@qti.qualcomm.com>
2025-11-05 12:29:14 +0000

    feat(qti): port SMMU driver from QTISECLIB
    Port the SMMU driver from QTISECLIB and format it to
    prepare for upstreaming.
    
    Change-Id: I938ca39c0d268b59002cef14b71e6ba9d78a30b5
    Signed-off-by: Hailey Liney <hliney@qti.qualcomm.com>

--- plat/qti/kodiak/rb3gen2/platform.mk
+++ plat/qti/kodiak/rb3gen2/platform.mk
@@ -103,6 +103,12 @@ $(warning QTISECLIB_PATH is not provided while building, using stub implementati
 		Please refer to documentation for more details \
 		THIS FIRMWARE WILL NOT BOOT!)
 
+PLAT_QTI_SMMU_DRIVER	:=	1
+$(eval $(call add_define,PLAT_QTI_SMMU_DRIVER))
+
+BL31_SOURCES	+=	drivers/qti/smmu/smmu.c				\
+			drivers/qti/smmu/smmu_kodiak_cfg.c		
+
 include drivers/qti/accesscontrol/access_control.mk
 
 BL31_SOURCES	+=	plat/qti/qtiseclib/src/qtiseclib_interface_stub.c \
