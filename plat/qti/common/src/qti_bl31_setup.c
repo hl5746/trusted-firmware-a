998a47901d79e04610593a0ca7205bac0aedc96c
Hailey Liney <hliney@qti.qualcomm.com>
2025-11-05 12:29:14 +0000

    feat(qti): port SMMU driver from QTISECLIB
    Port the SMMU driver from QTISECLIB and format it to
    prepare for upstreaming.
    
    Change-Id: I938ca39c0d268b59002cef14b71e6ba9d78a30b5
    Signed-off-by: Hailey Liney <hliney@qti.qualcomm.com>

--- plat/qti/common/src/qti_bl31_setup.c
+++ plat/qti/common/src/qti_bl31_setup.c
@@ -16,6 +16,7 @@
 #include <drivers/qti/accesscontrol/xpu.h>
 #include <drivers/qti/qtimer/qtimer.h>
 #include <drivers/qti/sec_core/sec_core.h>
+#include <drivers/qti/smmu/smmu.h>
 #include <drivers/qti/watchdog/watchdog.h>
 #include <lib/bl_aux_params/bl_aux_params.h>
 #include <lib/coreboot.h>
@@ -100,7 +101,6 @@ void bl31_platform_setup(void)
 #endif
 	generic_delay_timer_init();
 
-	/* Initialize the GIC driver, CPU and distributor interfaces */
 	plat_qti_gic_driver_init();
 	plat_qti_gic_init();
 	qti_smmu_init();
