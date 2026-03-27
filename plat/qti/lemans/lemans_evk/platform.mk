2657992f8020002b2c2ed2dadc84b94e4dddcced
hliney <hliney@qti.qualcomm.com>
2026-03-27 16:19:35 +0000

    feat(plat/qti): add ring buffer console driver
    Introduce the QTI ring buffer implementation and console driver.
    This commit provides the common implementation and Lemans-specific
    configuration of the ring buffer used to hold diagnostic information
    read by Crashscope.
    
    Signed-off-by: Hailey Liney <hliney@qti.qualcomm.com>

--- plat/qti/lemans/lemans_evk/platform.mk
+++ plat/qti/lemans/lemans_evk/platform.mk
@@ -88,6 +88,7 @@ BL31_SOURCES		+=	drivers/delay_timer/generic_delay_timer.c		\
 				${GICV3_SOURCES}					\
 				plat/common/plat_psci_common.c				\
 				$(PLAT_PATH)/common/src/$(ARCH)/qti_helpers.S		\
+				$(PLAT_PATH)/common/src/$(ARCH)/qti_ringbuf_console.S	\
 				$(PLAT_PATH)/common/src/pm_ps_hold.c			\
 				$(PLAT_PATH)/common/src/qti_bl31_setup.c		\
 				$(PLAT_PATH)/common/src/qti_gic_v3.c			\
@@ -118,3 +119,4 @@ $(eval $(call add_define,QTISECLIB_PATH))
 LDFLAGS += -L $(dir $(QTISECLIB_PATH))
 LDLIBS += -l$(patsubst lib%.a,%,$(notdir $(QTISECLIB_PATH)))
 endif
+
